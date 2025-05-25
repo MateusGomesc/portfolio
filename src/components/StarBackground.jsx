13f4a07e6cf8b44abf3716d8ca4a0017511362d8
Mateus Gomes <mateuscostagomes000@gmail.com>
2025-05-25 16:42:38 -0300

    feat: add ThemeToogle component

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,5 @@
+export function StarBackground(){
+    return(
+        
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export function Home(){
     return(
-        <div className="min-h-screen ">
-            
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            <ThemeToggle/>
+            <StarBackground/>
         </div>
     )
 }
