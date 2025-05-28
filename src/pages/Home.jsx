f7f2e8f33588a25a8727de72e1a15a158f13ac8d
Mateus Gomes <mateuscostagomes000@gmail.com>
2025-05-28 14:36:49 -0300

    feat: add SkillsSection component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
+import { AboutSection } from "../components/AboutSection";
 import { HeroSection } from "../components/HeroSection";
 import { NavBar } from "../components/NavBar";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -11,6 +13,8 @@ export function Home(){
             <NavBar/>
             <main>
                 <HeroSection/>
+                <AboutSection/>
+                <SkillsSection/>
             </main>
         </div>
     )
